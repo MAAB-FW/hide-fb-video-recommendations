0178da46876710195080ff093c6a6a7e18b25e2b
Mohammad Aiyub Ali Bappy <128562152+MAAB-FW@users.noreply.github.com>
2025-12-17 19:04:12 +0600

    Bump version to 1.2 and update CSS selectors

--- hide-fb-video-recommendations.user.js
+++ hide-fb-video-recommendations.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Video Recommendations (Watch Feed Blocker)
 // @namespace    https://github.com/MAAB-FW/hide-fb-video-recommendations
-// @version      1.1
+// @version      1.2
 // @description  Hide video recommendations and other unwanted sections on Facebook, even when scrolling
 // @author       @maab-fw
 // @match        https://www.facebook.com/*
@@ -21,7 +21,8 @@
         li:nth-of-type(23) > div,
         .x1iyjqo2 > div:nth-of-type(1) > ul > li:nth-of-type(14) > div,
         .x1a8lsjc.x889kno.xso031l.x1q0q8m5.x1nb4dca.x1ve1bff,
-        li.x1gz44f.x13fj5qh.x1awlv9s.x1l38jg0.x1hxoosp.xmlsiyf.x1iyjqo2:nth-of-type(3) {
+        li.x1gz44f.x13fj5qh.x1awlv9s.x1l38jg0.x1hxoosp.xmlsiyf.x1iyjqo2:nth-of-type(3)
+        .x11t971q.xvc5jky.x1vqzy7e.x13qq4wz.xaooukx {
             display: none !important;
         }
     `;
